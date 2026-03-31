d4a897e70f1fedec83feb6b6042545212a835ef3
Sarvesh772 <godsarveshop@gmail.com>
2026-03-31 21:49:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const isMaintenancePage = document.body?.dataset.page === "maintenance";
+
+if (!isMaintenancePage) {
+  const maintenanceTarget = new URL("maintenance.html", window.location.href);
+  const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+  if (currentPage !== "maintenance.html") {
+    maintenanceTarget.searchParams.set("from", currentPage);
+    window.location.replace(maintenanceTarget.toString());
+  }
+}
+
 const navToggle = document.querySelector(".nav-toggle");
 const siteNav = document.querySelector(".site-nav");
 
